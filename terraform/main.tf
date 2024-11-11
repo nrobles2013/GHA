483f1f3b1eade646fcd531ea1601d340c90a06d1
Yul Nestor Robles Prieto <yr103482@POPULARSADOMAIN.LOCAL>
2024-11-10 20:08:24 -0500

    terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ provider "aws"{
 }
 
 resource "aws_s3_bucket" "example" {
-  bucket = "my-example-bucketYUL"
+  bucket = "my-example-bucketyul"
     tags = {
         Name = "My bucket_yul1"
         Environment = "Dev"
