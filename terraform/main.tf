30bed3ea244ba51246795640d74f66668a6a683a
Yul Nestor Robles Prieto <yr103482@POPULARSADOMAIN.LOCAL>
2024-11-10 20:27:19 -0500

    terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,12 +2,20 @@ provider "aws"{
     region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "example" {
+resource "aws_s3_bucket" "master_bucket" {
   bucket = "my-example-bucketyul"
     tags = {
         Name = "S3Bucket_terraform"
         Environment = "development"
     }
+}
+
+resource "aws_s3_bucket_versioning" "versioning_bucket_terraform" {
+  bucket =  aws_s3_bucket.master_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
 
 resource "aws_s3_bucket" "log_bucket" {
@@ -19,7 +27,7 @@ resource "aws_s3_bucket" "log_bucket" {
 }
 
 resource "aws_s3_bucket_logging" "bucket_logging" {
-  bucket =  aws_s3_bucket.example.id  
+  bucket =  aws_s3_bucket.master_bucket.id  
   target_bucket = aws_s3_bucket.log_bucket.id
   target_prefix = "/log/"
 }
